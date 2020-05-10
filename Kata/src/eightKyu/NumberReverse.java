0a523e6b4c8c98aa71379aa4cda651504767bf5f
Jiayu <jxz933@student.bham.ac.uk>
2020-05-10 15:16:30 +0100

    Minor change

--- Kata/src/eightKyu/NumberReverse.java
+++ Kata/src/eightKyu/NumberReverse.java
@@ -18,10 +18,10 @@ public class NumberReverse {
 
 	public static int[] digitize(int n) {
 		//return Arrays.repeat(n);
-		  String s = String.valueOf(n);
-		    int length = s.length();
+		  String s = String.valueOf(n); //35231
+		    int length = s.length(); //5
 		    int[] array = new int[length];
-		    for ( int i = 0; i < length; i++){
+		    for (int i = 0; i < length; i++){
 		      array[i] = (int) (s.charAt(length - i - 1)) - 48;
 		    }
 		    return array;
